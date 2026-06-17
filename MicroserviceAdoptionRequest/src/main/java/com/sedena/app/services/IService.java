80bc9986aff3139ea4337fc4c3b7c5a10d90a544
christian <yosoypro_117@hotmail.com>
2026-06-17 13:33:05 -0600

    feat(adoption): add microservice adoption request v1

---
+++ MicroserviceAdoptionRequest/src/main/java/com/sedena/app/services/IService.java
@@ -0,0 +1,12 @@
+package com.sedena.app.services;
+
+import java.util.List;
+
+import com.sedena.app.entities.AdoptionRequest;
+
+public interface IService {
+	boolean insert(long adopterId, long petId);
+	List<AdoptionRequest> findAll();
+	boolean delete(long id);
+	List<AdoptionRequest> findByEmail(String email);
+}
